6ccccd71293bf6940de7d5278acbe16fce9b9585
bruvini <86611700+bruvini@users.noreply.github.com>
2022-12-17 11:49:44 -0300

    montando o robô

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
-const somar = document.querySelector('#somar')
-const subtrair = document.querySelector('#subtrair')
-const braco = document.querySelector('#braco')
-
 const controle = document.querySelectorAll('.controle-ajuste')
 
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
-        manipulaDados(evento.target.textContent)
+        manipulaDados(evento.target.textContent, evento.target.parentNode)
     })
 })
 
-function manipulaDados (op) {
+function manipulaDados (op, controle) {
+    const peca = controle.querySelector('.controle-contador')
+    
     if (op === "-") {
-        braco.value = parseInt(braco.value) - 1
+        peca.value = parseInt(peca.value) - 1
     } else {
-        braco.value = parseInt(braco.value) + 1
+        peca.value = parseInt(peca.value) + 1
     }
 }
